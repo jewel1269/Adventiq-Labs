8233783443b215406c4c3621642a137d0b755e41
Jewel Mia <145703213+jewel1269@users.noreply.github.com>
2025-11-25 20:24:51 +0600

    remove button

--- src/components/hero-section/hero.tsx
+++ src/components/hero-section/hero.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 
 export default function HeroWavebox() {
